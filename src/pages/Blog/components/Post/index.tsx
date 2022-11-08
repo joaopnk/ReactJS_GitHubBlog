a5e928e8bdd021d54a1de35024972d3206476e32
João <joaopedronk22@gmail.com>
2022-11-08 20:37:21 -0300

    bugfix: máximo de linhas permitidas

--- src/pages/Blog/components/Post/index.tsx
+++ src/pages/Blog/components/Post/index.tsx
@@ -2,22 +2,47 @@ import { IPost } from "../..";
 import { relativeDateFormatter } from "../../../../utils/formater";
 import { PostContainer } from "./styles";
 
-interface PostProps{
-    post: IPost
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
+interface PostProps {
+  post: IPost;
 }
 
-export function Post({post}: PostProps){
-    const formattedDate = relativeDateFormatter(post.created_at);
+export function Post({ post }: PostProps) {
+  const formattedDate = relativeDateFormatter(post.created_at);
 
-    return (
-        <PostContainer to={`/post/${post.number}`}>
-            <div>
-                <strong>{post.title}</strong>
-                <span>{formattedDate}</span>
-            </div>
-            <p>
-                {post.body}
-            </p>
-        </PostContainer>
-    )
+  return (
+    <PostContainer to={`/post/${post.number}`}>
+      <div>
+        <strong>{post.title}</strong>
+        <span>{formattedDate}</span>
+      </div>
+      <div>
+        {/* {post.body} */}
+        <ReactMarkdown
+          children={post.body}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, "")}
+                  style={dracula as any}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        />
+      </div>
+    </PostContainer>
+  );
 }
